f06606fc70104fb29f6fa8c6ed19b3f2cb1d3aec
nmelgar <nmelgar16013@gmail.com>
2022-10-04 12:46:31 -0500

    Added main function.

--- main_game.py
+++ main_game.py
@@ -1,37 +1,43 @@
-from colorama import init, Fore 
-import random 
+from colorama import init, Fore
+import random
 
-init(autoreset=True) #this stops the color to just adjust to one print statement 
 
-print(Fore.MAGENTA + r"""\
-             _____   ___  ____________   _____   ___  ___  ___ _____ 
-            /  __ \ / _ \ | ___ \  _  \ |  __ \ / _ \ |  \/  ||  ___|
-            | /  \// /_\ \| |_/ / | | | | |  \// /_\ \| .  . || |__  
-            | |    |  _  ||    /| | | | | | __ |  _  || |\/| ||  __| 
-            | \__/\| | | || |\ \| |/ /  | |_\ \| | | || |  | || |___ 
-             \____/\_| |_/\_| \_|___/    \____/\_| |_/\_|  |_/\____/ 
-                                                            
-    """)
- 
+# this stops the color to just adjust to one print statement
+# init(autoreset=True)
+
+# print(Fore.MAGENTA + r"""\
+#              _____   ___  ____________   _____   ___  ___  ___ _____
+#             /  __ \ / _ \ | ___ \  _  \ |  __ \ / _ \ |  \/  ||  ___|
+#             | /  \// /_\ \| |_/ / | | | | |  \// /_\ \| .  . || |__
+#             | |    |  _  ||    /| | | | | | __ |  _  || |\/| ||  __|
+#             | \__/\| | | || |\ \| |/ /  | |_\ \| | | || |  | || |___
+#              \____/\_| |_/\_| \_|___/    \____/\_| |_/\_|  |_/\____/
+
+#     """)
+
+
+def main():
+    start_game = int(input("\nPlease enter 1 to play or 2 to exit: "))
+    if start_game == 1:
+        game_interface()
+
+    else:
+        print("Okay goodbye")
+
 
 def game_interface():
-    
-    while True: 
-        print("The card is: {}")
+    run = True
+    while run:
+        print("The card is: ")
         #random_decision = input(print("Higher or Lower? [h/l] "))
         print("Next card was: ")
-        print("Your score is: {}")
-        play_again = str(input(print("Play again? [y/n] ")))
-        if play_again =="y":
+        print("Your score is: ")
+        play_again = input("Play again? [y/n] ")
+        if play_again == "y":
             game_interface()
-        else: 
-            pass
+        elif play_again == "n":
+            run = False
 
 
-
-start_game = input(print("Please enter 1 to play press 2 to exit"))
-if start_game == "1": 
-    game_interface()
-
-else: 
-    print("Okay goodbye")
+if __name__ == "__main__":
+    main()
